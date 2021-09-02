1b7f83c4d59d075e403317fc0e72979a0e1a55e3
Zachary Gomez <zgomez301@gmail.com>
2021-09-01 18:24:38 -0700

    moved back to fetch and added constants

--- js/script.js
+++ js/script.js
@@ -1,7 +1,17 @@
 //Constants and Variables
 
-
-
+const $input = $('input[type="text"]')
+const $name = $('#name')
+const $ages = $('#ages')
+const $location = $('#location')
+const $address = $('#address')
+const $state = $('#state')
+const $date = $('#date')
+const $startTime = $('#startTime')
+const $endTime = $('#endTime')
+const $latitude = $('#latitude')
+const $longitude = $('#longitude')
+const $artistlist = $('#artistlist')
 
 const API_KEY = ""
 const BASE_URL =  "https://edmtrain.com/api/events?"
@@ -49,29 +59,44 @@ const timePSD = (epoch) => {
 // console.log(timePSD(1629939676))
 
 // API Call
-
-//Was attempting to use fetch
-// function handleGetData(event) {
-//     event.preventDefault();
-//     console.log("Form Submitted");
-//     userInput = $input.val();
-//     fetch(`${BASE_URL}location${$input.val()}&client=${API_KEY}`)
-//         .then((response => {
-//             return response.json()
-//         })
-//         .then((data) => {         
-//         });
-//         .catch(error(); => {
-//         })
-
-function handleGetData(event){
+// ${BASE_URL}location${$input.val()}&client=${API_KEY}
+// Was attempting to use fetch
+function handleGetData(event) {
     event.preventDefault();
     console.log("Form Submitted");
     userInput = $input.val();
-    $.ajax({
-        url: `${BASE_URL}location${$input.val()}&client=${API_KEY}`
-    }).then(
-        function(data){
-            
-        }
-    )
+    fetch(`https://edmtrain.com/api/events?artistIds=195&client=324716fa-68fa-4c4d-8ae2-d6e384294662`)
+        .then((response => {
+            return response.json();
+        }))
+        .then(function(data) { 
+            console.log(data);
+            let edm = data.data
+            console.log(edm)        
+        })
+    .catch(function(error) {
+        console.log('bad request: ', error);
+    })
+    }
+
+// function handleGetData(event){
+//     event.preventDefault();
+//     console.log("Form Submitted");
+//     userInput = $input.val();
+//     $.ajax({
+//         url: `https://edmtrain.com/api/events?artistIds=195&client=324716fa-68fa-4c4d-8ae2-d6e384294662`
+//         // url: `${BASE_URL}location${$input.val()}&client=${API_KEY}`
+//         }).then(
+//             function(data){
+//             console.log(data);
+//             },
+//             function(error){
+//             console.log('bad request: ', error);
+//             });
+// }
+
+
+
+
+
+$('form').on('submit', handleGetData)
