68f82f89d74176c41da551463c1d5ffe28faa525
Zachary Gomez <zgomez301@gmail.com>
2021-09-05 23:00:30 -0700

    menu and formatting

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 ////////////////
 //Constants and Variables
 ////////////////
-const $input = $('input[type="text"]')
 const $name = $('#name')
 const $ages = $('#ages')
 const $location = $('#location')
@@ -14,10 +13,17 @@ const $latitude = $('#latitude')
 const $longitude = $('#longitude')
 const $artistList = $('#artistList')
 
-const API_KEY = ""
-const BASE_URL =  "https://edmtrain.com/api/events?"
+//Input information 
+const $input = $('input[type="text"]')
+
+
+const API_KEY1= ""
+const BASE_URL1 = "https://www.mapquestapi.com/geocoding/v1/batch?&inFormat=kvp&outFormat=json&thumbMaps=false&maxResults=1&location="
 const lat = ""
 const long = ""
+
+const API_KEY2= "324716fa-68fa-4c4d-8ae2-d6e384294662"
+const BASE_URL2 =  "https://edmtrain.com/api/events?"
 const NEAR_EVENT=  `latitude=${lat}&longitude=${long}`
 
 ////////////////
@@ -33,13 +39,13 @@ $('#hiddenArtists').hide()
 // Locations API.
 // locations?state=Nevada&city=Las%20Vegas&client=
 
-// API Errors.
-// {
-//     "data": [],
-//     "message": "Location not found",
-//     "success": false
-//  }
-
+////////////////
+// Pulldown and Search fields
+////////////////
+const changemenu = () => {
+    const $pulldown = $("#pulldown :selected").val(); 
+    console.log($pulldown)
+}
 
 ////////////////
 // Functions
@@ -65,12 +71,22 @@ const timePSD = (epoch) => {
 ////////////////
 // ${BASE_URL}location${$input.val()}&client=${API_KEY}
 
+function handleGetLatLon(loc) {
+    loc.preventDefault();
+    console.log($input)
+    console.log($input.val())
+    if ($("#pulldown :selected").val() == 3) {
+        console.log("Location Received");
+        fetch('https://www.mapquestapi.com/geocoding/v1/batch?&inFormat=kvp&outFormat=json&thumbMaps=false&maxResults=1&location=Camarillo CA&location=New York&location=Austin, TX&key=KEY')
+    }
+    handleGetData();
+}
+
 // Was attempting to use fetch, got it to work ^.^ s
 function handleGetData(event) {
-    event.preventDefault();
     console.log("Form Submitted");
     userInput = $input.val();
-    fetch(``)
+    fetch(`${BASE_URL2}locationIds=69&startDate=2021-09-14&client=${API_KEY2}`)
         .then((response => {
             return response.json();
         }))
@@ -96,7 +112,7 @@ function handleGetData(event) {
 
     
 $("#btnArt").on('click', () => {$('#hiddenArtists').toggle()})
-$('form').on('submit', handleGetData)
+$('form').on('submit', handleGetLatLon)
     ////////////////
     //Keeping in case fetch fails
     //Stop trying to make fetch a thing!
